09545a5cdbb3af8acde50c8759fb72ecb6915846
guruias123 <74144270+guruias123@users.noreply.github.com>
2021-09-19 16:58:05 +0530

    a

--- Router/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const Sequelize = require('sequelize')
-const route = express.Router();
-
-const translateController = require('../Controllers/translate');
-
-route.post('/translate', translateController.getTranslatedResponse);
-
-module.exports = route ;
